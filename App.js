4b7d461baf198e9623ecabc1667c10a4cc1e61ab
kpose <ganihujude@gmail.com>
2021-02-04 06:46:28 +0100

    bootstrap app and install libs

---
+++ App.js
@@ -0,0 +1,25 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Text>Welcome screen</Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
